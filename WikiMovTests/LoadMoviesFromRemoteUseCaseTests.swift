6a6f85737e5c43977186e10c9603e6f11cd48d0b
Bayu Kurniawan <kurniawan.bayu@hotmail.com>
2021-10-31 23:42:51 +0700

    Test load should request data from URL

--- WikiMovTests/LoadMoviesFromRemoteUseCaseTests.swift
+++ WikiMovTests/LoadMoviesFromRemoteUseCaseTests.swift
@@ -4,23 +4,71 @@
 import XCTest
 import WikiMov
 
-protocol HTTPClient {
-  typealias Result = Swift.Result<(Data, HTTPURLResponse), Error>
+class DefaultMovieLoader: MovieLoader {
+  
+  private let client: HTTPClient
+  private let endpoint: Endpoint
+  private var request: URLRequest!
+  
+  init(client: HTTPClient, endpoint: Endpoint) throws {
+    self.client = client
+    self.endpoint = endpoint
+    self.request = try endpoint.makeURLRequest()
+  }
+  
+  typealias Result = MovieLoader.Result
+  
+  func load(completion: @escaping (Result) -> Void) {
+    client.get(from: request, queue: .main) { _ in
+}
+  }
 }
 
-class HTTPClientSpy {
-  private var messages = [(url: URL, completion: (HTTPClient.Result) -> Void)]()
+protocol HTTPClient {
+  typealias Result = Swift.Result<(Data, HTTPURLResponse), Error>
   
-  var requestedURLs: [URL] {
-    return messages.map { $0.url }
+  func get(from request: URLRequest, queue: DispatchQueue, completion: @escaping (Result) -> Void)
+}
+
+class HTTPClientSpy: HTTPClient {
+  
+  private var messages = [(request: URLRequest, completion: (HTTPClient.Result) -> Void)]()
+  
+  var requestedURLs: [URLRequest] {
+    return messages.compactMap { $0.request }
+  }
+  
+  func get(from request: URLRequest, queue: DispatchQueue = .main, completion: @escaping (HTTPClient.Result) -> Void) {
+    messages.append((request, completion))
   }
 }
 
 class LoadMoviesFromRemoteUseCaseTests: XCTestCase {
   
   func test_init_doesNotRequestDataFromURL() {
-    let client = HTTPClientSpy()
+    let (_, client) = makeSUT()
     
     XCTAssertTrue(client.requestedURLs.isEmpty)
   }
+  
+  func test_load_shouldRequestDataFromURL() throws {
+    
+    let endpoint = try MovieEndpoint.popular.makeURLRequest()
+    let (sut, client) = makeSUT()
+    
+    sut.load { _ in }
+    
+    XCTAssertEqual(client.requestedURLs, [endpoint])
+  }
+  
+  // MARK: - Helpers
+  private func makeSUT(
+    endpoint: Endpoint = MovieEndpoint.popular,
+      file: StaticString = #filePath,
+      line: UInt = #line) -> (DefaultMovieLoader, HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = try! DefaultMovieLoader(client: client, endpoint: endpoint)
+        
+        return (sut, client)
+      }
 }
