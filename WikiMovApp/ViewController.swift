6c3474359c259ee9b2c25f6cba0da3055e2f29d5
Bayu Kurniawan <kurniawan.bayu@hotmail.com>
2021-11-01 01:47:22 +0700

    Add WikiMovApp module

---
+++ WikiMovApp/ViewController.swift
@@ -0,0 +1,15 @@
+// Created for WikiMov. By @overheardswift.
+// Copyright © 2021. All rights reserved.
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
