e380ceb6a071f659c5bd591bd37c67b6cc11ccd2
Bayu Kurniawan <kurniawan.bayu@hotmail.com>
2021-11-01 01:40:28 +0700

    Create URLSessionHTTPClient

---
+++ WikiMov/Movie API/URLSessionHTTPClient.swift
@@ -0,0 +1,27 @@
+// Created for WikiMov. By @overheardswift.
+// Copyright © 2021. All rights reserved.
+
+import Foundation
+
+public final class URLSessionHTTPClient: HTTPClient {
+  
+  private let session: URLSession
+  
+  init(session: URLSession = .shared) {
+    self.session = session
+  }
+  
+  public func get(from request: URLRequest, queue: DispatchQueue, completion: @escaping (HTTPClient.Result) -> Void) {
+    session.dataTask(with: request) { data, response, error in
+      if let error = error {
+        queue.async {
+          completion(.failure(error))
+        }
+      } else if let data = data, let response = response as? HTTPURLResponse {
+        queue.async {
+          completion(.success((data, response)))
+        }
+      }
+    }.resume()
+  }
+}
