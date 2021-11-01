a0ab8c6ff02fd1b55172e01f561e1f3efb29d729
Bayu Kurniawan <kurniawan.bayu@hotmail.com>
2021-11-01 22:35:12 +0700

    Add LoadFavoriteMoviesFromLocalStoreUseCases tests

---
+++ WikiMovTests/Helpers/LocalMovieStoreSpy.swift
@@ -0,0 +1,78 @@
+// Created for WikiMov. By @overheardswift.
+// Copyright © 2021. All rights reserved.
+
+import WikiMov
+import Foundation
+
+func uniqueMachineData() -> LocalMovie {
+  return LocalMovie(id: randomNumber(6), title: "A title", backdropPath: "/\(randomNumber(10))", posterPath: "\(randomNumber(10))", releaseDate: "2021-05-20", overview: "An overview", isFavorite: true)
+}
+
+func randomNumber(_ digits: Int) -> Int {
+  let min = Int(pow(Double(10), Double(digits-1))) - 1
+  let max = Int(pow(Double(10), Double(digits))) - 1
+  return Int.random(in: min...max)
+}
+
+func anyNSError() -> NSError {
+  return NSError(domain: "Any Error", code: 0)
+}
+
+final class MachineStoreSpy: LocalMovieStore {
+  
+  enum ReceivedMessage: Equatable {
+    case retrieve
+    case insert(LocalMovie)
+    case delete(LocalMovie)
+  }
+  
+  private(set) var insertionCompletions = [InsertionCompletion]()
+  private(set) var retrievalCompletions = [RetrievalCompletion]()
+  private(set) var removalCompletion = [RemovalCompletion]()
+  
+  private(set) var receivedMessages = [ReceivedMessage]()
+  
+  func insert(_ data: LocalMovie, completion: @escaping InsertionCompletion) {
+    receivedMessages.append(.insert(data))
+    insertionCompletions.append(completion)
+  }
+  
+  func completeInsertion(with error: Error, at index: Int = 0) {
+    insertionCompletions[index](error)
+  }
+  
+  func completeInsertionSuccessfully(at index: Int = 0) {
+    insertionCompletions[index](nil)
+  }
+  
+  func retrieve(completion: @escaping (RetrieveCacheDataResult) -> Void) {
+    self.receivedMessages.append(.retrieve)
+    self.retrievalCompletions.append(completion)
+  }
+  
+  func completeRetrieval(with error: Error, at index: Int = 0) {
+    self.retrievalCompletions[index](.failure(error))
+  }
+  
+  func completeRetrieval(with data: [LocalMovie], at index: Int = 0) {
+    self.retrievalCompletions[index](.found(data))
+  }
+  
+  func completeRetrievalWithEmptyCache(at index: Int = 0) {
+    self.retrievalCompletions[index](.empty)
+  }
+  
+  func remove(_ data: LocalMovie, completion: @escaping RemovalCompletion) {
+    self.removalCompletion.append(completion)
+    self.receivedMessages.append(.delete(data))
+  }
+  
+  func completeDeletion(with error: Error, at index: Int = 0) {
+    self.removalCompletion[index](error)
+  }
+  
+  func completeDeletionSuccessfully(at index: Int = 0) {
+    self.removalCompletion[index](nil)
+  }
+}
+
---
+++ WikiMovTests/LoadFavoriteMoviesFromLocalStoreUseCases.swift
@@ -0,0 +1,95 @@
+// Created for WikiMov. By @overheardswift.
+// Copyright © 2021. All rights reserved.
+
+import XCTest
+import WikiMov
+
+class LoadFavoriteMoviesFromLocalStoreUseCases: XCTestCase {
+  
+  func test_init_doesNotMessageStoreUponCreation() {
+    let (_, store) = makeSUT()
+    
+    XCTAssertEqual(store.receivedMessages, [])
+  }
+  
+  func test_load_requestsCacheRetrieval() {
+    let (sut, store) = makeSUT()
+    
+    sut.load { _ in }
+    
+    XCTAssertEqual(store.receivedMessages, [.retrieve])
+  }
+  
+  func test_load_failsOnRetrievalError() {
+    let (sut, store) = makeSUT()
+    let retrievalError = anyNSError()
+   
+    expect(sut, toCompleteWith: .failure(retrievalError)) {
+      store.completeRetrieval(with: retrievalError)
+    }
+  }
+  
+  func test_load_deliversNoDataOnEmptyCache() {
+    let (sut, store) = makeSUT()
+    
+    expect(sut, toCompleteWith: .success([])) {
+      store.completeRetrievalWithEmptyCache()
+    }
+  }
+  
+  func test_load_deliversDataOnNonEmptyCache() {
+    let (sut, store) = makeSUT()
+    let data = [uniqueMachineData(), uniqueMachineData()]
+    
+    expect(sut, toCompleteWith: .success(data)) {
+      store.completeRetrieval(with: data)
+    }
+  }
+  
+  func test_load_doesNotDeliverResultAfterSUTInstanceHasBeenDeallocated() {
+    let store = MachineStoreSpy()
+    var sut: DefaultLocalMovieLoader? = DefaultLocalMovieLoader(store: store)
+    
+    var receivedResult = [LoadDataResult]()
+    sut?.load { receivedResult.append($0) }
+    
+    sut = nil
+    store.completeRetrievalWithEmptyCache()
+    
+    XCTAssertTrue(receivedResult.isEmpty)
+  }
+  
+  // MARK: -  Helpers
+  private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: DefaultLocalMovieLoader, store: MachineStoreSpy) {
+    let store = MachineStoreSpy()
+    let sut = DefaultLocalMovieLoader(store: store)
+    trackForMemoryLeaks(for: store, file: file, line: line)
+    trackForMemoryLeaks(for: sut, file: file, line: line)
+    
+    return (sut, store)
+  }
+  
+  private func expect(_ sut: DefaultLocalMovieLoader, toCompleteWith expectedResult: LoadDataResult, when action: () -> Void, file: StaticString = #filePath, line: UInt = #line) {
+    
+    let exp = expectation(description: "Wait for retrieval completion")
+    
+    sut.load { receivedResult in
+      switch (receivedResult, expectedResult) {
+      case let (.success(receivedData), .success(expectedData)):
+        XCTAssertEqual(receivedData, expectedData, file: file, line: line)
+      case let (.failure(receivedError as NSError?), .failure(expectedError as NSError?)):
+        XCTAssertEqual(receivedError, expectedError, file: file, line: line)
+      default:
+        XCTFail("Expected \(expectedResult), got \(receivedResult) instead.")
+      }
+      exp.fulfill()
+    }
+    
+    action()
+    
+    wait(for: [exp], timeout: 1.0)
+  }
+  
+ 
+}
+
