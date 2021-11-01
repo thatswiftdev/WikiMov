f3bae3b29282b76510d940250e9b1bb96d24e51a
Bayu Kurniawan <kurniawan.bayu@hotmail.com>
2021-11-01 08:41:28 +0700

    Create constant file for images, colors, and string resources

---
+++ WikiMovApp/Helpers/Constants.swift
@@ -0,0 +1,18 @@
+// Created for WikiMov. By @overheardswift.
+// Copyright © 2021. All rights reserved.
+
+import UIKit
+
+struct Constants {
+  struct Image {
+    static let favorite = UIImage(systemName: "heart.fill")
+  }
+  
+  struct Color {
+    static let pink = UIColor.systemPink
+  }
+  
+  struct App {
+    static let title = "WikiMov"
+  }
+}
