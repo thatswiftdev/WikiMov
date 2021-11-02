fa8e07c9fae154c2057c26e1e86e24a521e14129
Bayu Kurniawan <kurniawan.bayu@hotmail.com>
2021-11-02 08:13:07 +0700

    Adding category button

--- WikiMovApp/Presentation/Movie List/Controller/MovieListViewController.swift
+++ WikiMovApp/Presentation/Movie List/Controller/MovieListViewController.swift
@@ -21,9 +21,20 @@ class MovieListViewController: UIViewController {
   }
   
   private lazy var scrollView = ScrollViewContainer.make {
-    $0.edges(to: view, 0, true)
+    $0.top(to: view, of: .top(true), 0)
+    $0.horizontalPadding(to: view)
     $0.setBackgroundColor(color: .white)
     $0.setSpacingBetweenItems(to: 5)
+  }
+  
+  private lazy var categoryButton = UIButton.make {
+    $0.height(40)
+    $0.horizontalPadding(to: view)
+    $0.bottom(to: view, of: .bottom(true), 0)
+    $0.setTitle("Category", for: .normal)
+    $0.setTitleColor(.white, for: .normal)
+    $0.backgroundColor = Constants.Color.pink
+    $0.titleLabel?.font = .systemFont(ofSize: 13, weight: .semibold)
   }
   
   private lazy var tableView = UITableView.make {
@@ -36,7 +47,7 @@ class MovieListViewController: UIViewController {
     super.viewWillAppear(animated)
     tableView.addObserver(self, forKeyPath: UITableView.contentSizeKeyPath, options: .new, context: nil)
   }
-
+  
   override func viewWillDisappear(_ animated: Bool) {
     super.viewWillDisappear(animated)
     tableView.removeObserver(self, forKeyPath: UITableView.contentSizeKeyPath)
@@ -69,9 +80,10 @@ class MovieListViewController: UIViewController {
   private func configureSubviews() {
     tableViewHeight = tableView.heightAnchor.constraint(equalToConstant: 0)
     view.addSubviews([
-      scrollView.addArrangedSubViews([
-        tableView
-      ])
+      scrollView.addArrangedSubViews([tableView]),
+      categoryButton
+    ]).addCustomConstraints([
+      .spacing(from: scrollView, .bottom(false), relation: .equal, to: categoryButton, .top(false))
     ])
   }
   
@@ -83,7 +95,7 @@ class MovieListViewController: UIViewController {
   }
   
   private func updateTableViewContentSize(size: CGFloat) {
-    animate(with: scrollView) {
+    animate(with: tableView) {
       self.tableViewHeight?.constant = size
       self.view.layoutIfNeeded()
     } completion: { _ in }
